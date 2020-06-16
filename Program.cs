f81f705f25328fbc6369b40f9c1ea9f2cd066011
Nyasia Williams <ny.williams123@gmail.com>
2020-06-16 12:52:40 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Lab2_5VarSyntaxInputOutputConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
