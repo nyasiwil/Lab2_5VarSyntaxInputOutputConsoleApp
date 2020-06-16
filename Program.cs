485f1e5aaaadb503f6d42aadba550cfa1f1e8287
Nyasia Williams <ny.williams123@gmail.com>
2020-06-16 14:03:07 -0400

    Completed  all 4 tasks. Version 1.0.

--- Program.cs
+++ Program.cs
@@ -13,6 +13,7 @@ namespace Lab2_5VarSyntaxInputOutputConsoleApp
             //and dividing of two numbers which will be entered by the user.
             int intFirstNumber = 0;
             int intSecondNumber = 0;
+            int intThirdNumber = 0;
             int intResult = 0;
             double dblResult = 0;
 
@@ -21,6 +22,10 @@ namespace Lab2_5VarSyntaxInputOutputConsoleApp
 
             Console.WriteLine("Enter your Second Number: ");
             intSecondNumber = Convert.ToInt32(Console.ReadLine());
+
+            //Not used until Task 3
+            Console.WriteLine("Enter your Third Number: ");
+            intThirdNumber = Convert.ToInt32(Console.ReadLine());
 
             //Adding
             intResult = intFirstNumber + intSecondNumber;
@@ -31,8 +36,8 @@ namespace Lab2_5VarSyntaxInputOutputConsoleApp
             Console.WriteLine($"{intFirstNumber} minus {intSecondNumber} is {intResult}"); 
 
             //Multiplying
-            intResult = intFirstNumber * intSecondNumber;
-            Console.WriteLine($"{intFirstNumber} multiplied by {intSecondNumber} is {intResult}");
+            intResult = intFirstNumber * intSecondNumber * intThirdNumber;
+            Console.WriteLine($"{intFirstNumber} multiplied by {intSecondNumber} multiplied by {intThirdNumber} is {intResult}");
 
             //Dividing
             dblResult = intFirstNumber / Convert.ToDouble(intSecondNumber);
@@ -40,12 +45,24 @@ namespace Lab2_5VarSyntaxInputOutputConsoleApp
 
 
             // 2. Write a C# program to print Hello and your name (which you will type in on the Console) in a separate line.
-
+            string personsName = string.Empty;
+            Console.WriteLine("Please enter your name: ");
+            personsName = Console.ReadLine();
+            Console.WriteLine($"Hello, {personsName}");
 
             // 3. Write a C# program to print the output of multiplication of three numbers which will be entered by the user.
+            int Result = intFirstNumber * intSecondNumber * intThirdNumber;
+            Console.WriteLine($"{intFirstNumber} multiplied by {intSecondNumber} multiplied by {intThirdNumber} is {intResult}");
 
-            // 4. Write a C# program that takes an age (for example 20) as input and prints something like "You lo
-            // 4. Write a C# program that takes an age (for example 20) as input and prints something like "You look younger than 20 (the age the user entered)".
+
+            // 4. Write a C# program that takes an age (for example 20) as input and prints something like
+            //"You look younger than 20 (the age the user entered)".
+            int intAge = 0;
+            Console.WriteLine("Please enter your age: ");
+            intAge = int.Parse(Console.ReadLine());
+            //OR
+            //intAge = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"You look younger than {intAge}.");
 
 
 
